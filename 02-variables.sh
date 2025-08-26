18b625b4f3432dae1f2ddb0b16cbb35c624bd68d
manoj515876 <manojputtaluckey@gmail.com>
2025-08-26 14:32:06 +0530

    Removed variables names

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-PERSON1=Manoj # no space before and after equal and Variables name must be captial letters.
-PERSON2=Sai
-echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hello $PERSON1"
-echo "$PERSON1:: How are you doing?"
-echo "$PERSON2:: I am good. How are you?"
+
+echo "Manoj:: Hi Mantha"
+echo "Mantha:: Hello Manoj"
+echo "Manoj:: How are you doing?"
+echo "Mantha:: I am good. How are you?"
