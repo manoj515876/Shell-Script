b54650be82eccefaefbf9bb814afb75807bff6fd
manoj515876 <manojputtaluckey@gmail.com>
2025-08-26 14:48:10 +0530

    ask username and password

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Enter username : " # text entered here will be added as value to variable
+echo "Please enter username" # text entered here will be added as value to variable
 
 read -s USERNAME # Here USERNAME is the variable name
 
 echo "Your username is $USERNAME"
+
+echo "Please enter password:"
+
+read -s PASSWORD
+
 
