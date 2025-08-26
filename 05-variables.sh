d0a3fd5ca1cf02d44ff0427887bba0888f1d6ad9
manoj515876 <manojputtaluckey@gmail.com>
2025-08-26 14:45:05 +0530

    ask username to user

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter username : " # text entered here will be added as value to variable
+
+read USERNAME # Here USERNAME is the variable name
+
+echo "Your username is $USERNAME"
+
