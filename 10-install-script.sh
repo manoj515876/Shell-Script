bd80094e5687e91f92c73a7909a075f824111ddd
manoj515876 <manojputtaluckey@gmail.com>
2025-08-27 19:45:30 +0530

    Git and Mysql packages install process program

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
 
-USERID=$(id -u)
+
+# Checking root user or not
+
+USERID=$(id -u) # (id -u) -> this command will give user id, If user id 0 he is a root user or else not .
 
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: You must have sudo access to execute this script"
 fi
 
-dnf install mysqll -y
+# mysql Package Installition Process
 
-dnf install git -y
+dnf list installed mysql 
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing MYSQL .... Failure"
+    else
+        echo "Instaling MYSQL .... SUCCESS"
+    fi
+else 
+    echo "MySQL already Installed ..."
+
+
+# Git Package Installition Process
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then 
+    dnf install git 
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing Git ... Faliure"
+    else    
+        echo "Installing Git ... SUCCESS"
+    fi
+else
+    echo "Git already Installed ..."
