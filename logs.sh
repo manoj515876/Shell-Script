4b0f05615616e4c1817596ab9d022c94652ed84d
manoj515876 <manojputtaluckey@gmail.com>
2025-08-28 10:40:45 +0530

    logs file added

---
+++ logs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +-%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE/$TIMESTAMP.log"
+
+VALIDATE(){
+    
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 .... $R Failure $N"
+        exit 1
+    else 
+        echo -e "$2 .... $G SUCCESS $N"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR:: You must have sudo access to execute this script" 
+    exit 1
+fi
+
+dnf list installed mysql &>>LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y &>>LOG_FILE_NAME
+    VALIDATE $? "Installing MYSQL"
+else 
+    echo -e "MySQL already $Y Installed $N"
+fi
+
+
+dnf list installed git &>>LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y &>>LOG_FILE_NAME
+    VALIDATE $? "Installing Git"
+else 
+    echo -e "Git already $Y Installed $N"
+fi
+
+
