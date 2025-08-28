b7c948acb230a60875ac5cd88d93579c361bd9a5
manoj515876 <manojputtaluckey@gmail.com>
2025-08-28 10:08:03 +0530

    functions file added

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]
+then 
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1
+fi
+
+dnf list installed mysql 
+
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing MYSQL .... Failure"
+        exit 1
+    else 
+        echo "Instaling MYSQL .... SUCCESS"
+else 
+    echo "MySQL already Installed ..."
+
+
