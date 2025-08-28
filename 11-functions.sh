acacedc6e5af87ca86889494a8be3ddbc9930c72
manoj515876 <manojputtaluckey@gmail.com>
2025-08-28 10:12:08 +0530

    functions file added

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then 
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1
@@ -21,5 +21,22 @@ then
         echo "Instaling MYSQL .... SUCCESS"
 else 
     echo "MySQL already Installed ..."
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    dnf install git -y 
+    if [ $? -ne 0 ]
+    then 
+        echo "Installing Git ... Failure"
+        exit 1;
+    else 
+        echo "Installing Git ... SUCCESS"
+    fi 
+else 
+    echo "Git already Installed ..."
+fi
 
 
