afab1c0dbd7c3735f4b297b4776b270b41482421
manoj515876 <manojputtaluckey@gmail.com>
2025-09-03 00:26:32 +0530

    Adding backup script

--- 18-backup.sh
+++ 18-backup.sh
@@ -28,4 +28,14 @@ then
     USAGE
 fi 
 
+if [ ! -d "$SOURCE_DIR" ]
+then 
+    echo -e "$SOURCE_DIR Does not exist...Please check"
+    exit 1 
+fi 
 
+if [ ! -d "$DEST_DIR" ]
+then 
+    echo -e "$DEST_DIR Does not exist...Please check"
+    exit 1
+fi
