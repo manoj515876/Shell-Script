4b998b015b4ed6fe0d4eac9163a5859b4f42c93c
manoj515876 <manojputtaluckey@gmail.com>
2025-09-03 00:24:08 +0530

    Adding backup script

---
+++ 18-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}  # if user is not providing number of days, we are taking 14 as default
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+USAGE(){
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+}
+
+if [ $# -ne 2 ]
+then 
+    USAGE
+fi 
+
+
