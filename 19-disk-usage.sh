d6cf89b2afcd375c238f7e99d279c63c5a04196a
manoj515876 <manojputtaluckey@gmail.com>
2025-09-07 17:30:44 +0530

    Disk Usage Script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -10,7 +10,7 @@ do
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then 
-        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE <br> "
     fi
 done <<< $DISK_USAGE
 
